6aa4b73f0808ed18934fdc073ab4a5e561229b08
David Kremer <david.kremer@sewan.fr>
2016-11-12 21:02:43 +0100

    updated content for the counter.go file

--- counter/counter.go
+++ counter/counter.go
@@ -1,24 +1,35 @@
-package main
+package counter
 
 import (
 	"fmt"
+	"log"
+	"os"
 )
 
 // count the number of occurence of each 8 bit sequence in a given string
-// return the result as
-func countOccurences(s string) [256]uint {
+// return the result as an array of unsigned integers
+// with the given setup, the actual limitation for this code to work is
+// that the size (in bytes) of the string does not exceed the capacity of
+// an unsigned int on the system (2**32 ~ 1 Gb on a 32 bytes system)
+//
+func CountOccurences(data []byte) [256]uint {
 	var occurences [256]uint
 
-	for _, value := range s {
+	for _, value := range data {
 		occurences[value] += 1
 	}
 	return occurences
 }
 
-func main() {
-	for k, v := range countOccurences("heeeeeeeeeeeeee") {
-		if v > 0 {
-			fmt.Println("%c : %u", k, v)
-		}
+func MakeDict(dict [256]uint, fname string) {
+	// write a file containing the frequency count for each possible value of
+	// a byte
+	dict_name := fname + ".dict"
+	file, err := os.Create(dict_name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for _, v := range dict {
+		fmt.Fprintln(file, v)
 	}
 }
