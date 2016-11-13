05288b694c5884db3275ba2e431c726d45643ed1
David Kremer <david.kremer@sewan.fr>
2016-11-14 00:07:51 +0100

    Restruct this file by keeping only the counting relative part in it

--- counter/counter.go
+++ counter/counter.go
@@ -1,10 +1,22 @@
 package counter
 
 import (
-	"fmt"
-	"log"
-	"os"
+	"sort"
 )
+
+type Pair struct {
+	Key   byte
+	Value uint64
+}
+
+// A slice of pairs that implements sort.Interface to sort by values
+type PairList []Pair
+
+func (p PairList) Len() int           { return len(p) }
+func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
+func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+
+type Dict map[byte]uint64
 
 // count the number of occurence of each 8 bit sequence in a given string
 // return the result as an array of unsigned integers
@@ -12,24 +24,30 @@ import (
 // that the size (in bytes) of the string does not exceed the capacity of
 // an unsigned int on the system (2**32 ~ 1 Gb on a 32 bytes system)
 //
-func CountOccurences(data []byte) [256]uint {
-	var occurences [256]uint
+func CountOccurences(data []byte) Dict {
+	var occurences Dict
 
 	for _, value := range data {
-		occurences[value] += 1
+		v, k := occurences[value]
+		if k {
+			occurences[value] = v + 1
+		} else {
+			occurences[value] = 1
+		}
 	}
 	return occurences
 }
 
-func MakeDict(dict [256]uint, fname string) {
-	// write a file containing the frequency count for each possible value of
-	// a byte
-	dict_name := fname + ".dict"
-	file, err := os.Create(dict_name)
-	if err != nil {
-		log.Fatal(err)
+func SortDict(data Dict) PairList {
+	Result := make(PairList, len(data))
+	for k, v := range data {
+		Result = append(Result, Pair{k, v})
 	}
-	for _, v := range dict {
-		fmt.Fprintln(file, v)
-	}
+	sort.Sort(Result)
+	return Result
+}
+
+func MakeDict(data []byte) PairList {
+	rawDict := CountOccurences(data)
+	return SortDict(rawDict)
 }
